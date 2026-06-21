dae989ec6eb94c1dcbb69a85d6554f4c2141aaaf
Mitchell Moore <mitchell@tcpit.co.uk>
2026-06-21 23:36:39 +0100

    Adding ability to use templates

--- HVA02/terraform/app.tf
+++ HVA02/terraform/app.tf
@@ -1,7 +1,8 @@
 module "virtual_machines" {
   for_each = var.virtual_machines
 
-  source = "git@github.com:mitchellmooreuk/terraform-modules.git//proxmox-vm?ref=v1.6.1"
+  # source = "git@github.com:mitchellmooreuk/terraform-modules.git//proxmox-vm?ref=v1.6.1"
+  source = "/home/mitch/Desktop/projects/terraform-modules/proxmox-vm"
 
   vm_name         = each.key
   vm_id           = each.value.vm_id
@@ -10,6 +11,8 @@ module "virtual_machines" {
   cpu_type        = each.value.cpu_type
   memory_mb       = each.value.memory_mb
   bios            = each.value.bios
+  clone_vm_id     = each.value.clone_vm_id
+  is_template     = each.value.is_template
   scsi_type       = each.value.scsi_type
   vm_description  = each.value.vm_description
   disks           = each.value.disks
