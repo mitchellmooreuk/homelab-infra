8ce2fc025251b1a03f712fbfc76b02f0672d831a
Mitchell Moore <mitchell@tcpit.co.uk>
2026-06-22 23:39:00 +0100

    Removing template references from app.tf

--- HVA02/terraform/app.tf
+++ HVA02/terraform/app.tf
@@ -1,7 +1,8 @@
 module "virtual_machines" {
   for_each = var.virtual_machines
 
-  source = "git@github.com:mitchellmooreuk/terraform-modules.git//proxmox-vm?ref=v1.7.0"
+  # source = "git@github.com:mitchellmooreuk/terraform-modules.git//proxmox-vm?ref=v1.7.0"
+  source = "/home/mitch/Desktop/projects/terraform-modules/proxmox-vm"
 
   vm_name         = each.key
   vm_id           = each.value.vm_id
@@ -10,11 +11,11 @@ module "virtual_machines" {
   cpu_type        = each.value.cpu_type
   memory_mb       = each.value.memory_mb
   bios            = each.value.bios
-  clone_vm_id     = each.value.clone_vm_id
-  is_template     = each.value.is_template
   scsi_type       = each.value.scsi_type
   vm_description  = each.value.vm_description
   disks           = each.value.disks
   cd_roms         = each.value.cd_roms
   network_devices = each.value.network_devices
+  ipv4_address    = each.value.ipv4_address
+  gateway         = each.value.gateway
 }
